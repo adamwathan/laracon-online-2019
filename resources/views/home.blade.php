aae9771f34448e484f68007c7ba19e1ba554b011
Adam Wathan <adam.wathan@gmail.com>
2019-03-05 14:53:50 -0500

    WIP

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Tailwind Travel</title>
+  </head>
+  <body class="bg-grey-2 font-sans leading-normal antialiased">
+    <header class="bg-cover bg-center" style="background-image: url('/img/hero-bg.jpg')">
+      <div class="max-w-5xl mx-auto pb-16">
+        <nav class="flex justify-between items-center py-5 border-b border-white-20">
+          <div class="flex items-center">
+            <svg class="h-10 w-10" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20z" fill="#fff"/><path d="M20 26.094l-8.594 1.82c-.857.38-1.72-.522-1.3-1.361l9-18a1 1 0 0 1 1.788 0l9 18c.42.839-.443 1.742-1.3 1.36L20 26.095z" fill="#6D9CCB"/><path d="M20 8c.355 0 .71.184.894.553l9 18c.42.839-.443 1.742-1.3 1.36L20 26.095V8z" fill="#16518C"/></svg>
+            <a href="#" class="ml-8 text-white no-underline font-bold">Flights</a>
+            <a href="#" class="ml-8 text-white no-underline font-bold">Hotels</a>
+            <a href="#" class="ml-8 text-white no-underline font-bold">Cars</a>
+            <a href="#" class="ml-8 text-white no-underline font-bold">Vacations</a>
+            <a href="#" class="ml-8 text-white no-underline font-bold">Deals</a>
+            <a href="#" class="ml-8 text-white no-underline font-bold">Cruises</a>
+          </div>
+          <div>
+            <a href="#" class="text-white no-underline font-bold">Trips</a>
+            <a href="#" class="inline-block ml-8 border border-white-20 rounded px-4 py-2 leading-tight text-white no-underline font-bold">
+              Sign in
+            </a>
+          </div>
+        </nav>
+        <div class="text-center py-16">
+          <h1 class="text-5xl text-white leading-none">Where will you be flying today?</h1>
+          <p class="mt-2 text-3xl text-blue-accent">Travel the world at prices you wouldn't believe.</p>
+        </div>
+        <div class="bg-white rounded-lg overflow-hidden shadow-xl -mb-48">
+          <div class="h-48">
+
+          </div>
+          <div class="bg-grey-1 h-20"></div>
+        </div>
+      </div>
+    </header>
+  </body>
+</html>
